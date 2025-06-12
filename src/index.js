b67c3ceb1316f1dee839555ffc5bba728b0ca0ff
kromer52 <ilonakromer52@googlemail.com>
2025-06-12 17:56:57 +0200

    removed comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function updateTime() {
-  // Los Angeles
+
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
     let losAngelesDateElement = losAngelesElement.querySelector(".date");
@@ -12,7 +12,7 @@ function updateTime() {
     );
   }
 
-  // Paris
+  
   let parisElement = document.querySelector("#paris");
   if (parisElement) {
     let parisDateElement = parisElement.querySelector(".date");
